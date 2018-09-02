b3c3bcb36e9de8a6402e0a15674898f7fc37fbc5
Cangyu Lin <jefflin.je598@gmail.com>
2018-09-02 22:59:21 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication4;
 
 import java.util.Arrays;
 
@@ -11,7 +5,7 @@ import java.util.Arrays;
  *
  * @author min20120907
  */
-public class JavaApplication4 {
+public class main {
 
     /**
      * @param args the command line arguments
